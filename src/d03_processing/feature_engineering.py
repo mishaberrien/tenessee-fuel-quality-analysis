a6fd89a8bc8e57caa7048fb950ac0e612430d91e
mishaberrien <markishab@gmail.com>
2019-07-31 05:20:37 -0400

    successfully migrated feature_entineering.py helper function to src/do3_processing

---
+++ src/d03_processing/feature_engineering.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+def process_data_for_model_building(gasoline, astm):
+    # turn datasampled to datetime
+    gasoline['datesampled'] = pd.to_datetime(gasoline['datesampled'])
+    # strip all astm columns of any white space
+    for col in astm.columns:
+        try:
+            astm[col] = astm[col].str.strip()
+        except AttributeError:
+            pass
+
+    # drop columns
+    gasoline.drop(columns=['zipcode'], inplace=True)
+    # drop na values
+    gasoline.dropna(subset=['grade'], inplace=True)
+    gasoline.dropna(inplace=True)
+
+    # prepare to join ASTM dataset to gasoline_proc set
+    gasoline['datesampled_month'] = gasoline.datesampled.dt.month
+    gasoline['datesampled_day'] = gasoline['datesampled'].dt.day
+    gasoline['datesampled_month_day'] = gasoline['datesampled_month'].astype('str') + '/' + gasoline['datesampled_day'].astype('str')
+    gasoline.rename(columns={'datesampled_month_day':'Date'}, inplace=True)
+
+    # join ASTM and gasoline_proc datasets
+    gasoline = gasoline.merge(astm,
+                                        how='left',
+                                        on='Date'
+                                       )
+    gasoline.reset_index(inplace=True, drop=True)
+    return gasoline
