5679b6d5c8e748686978aa535a228d226aaacd96
Kate H <katehayes.m51@gmail.com>
2019-08-02 13:23:42 -0400

    added returns and made the second function

--- notebooks/02_processing/Time_series_cleaning.py
+++ notebooks/02_processing/Time_series_cleaning.py
@@ -18,22 +18,35 @@ def volatilty_ASTM_df_creator(routine_csv, ASTM_csv):
     volitility_df.rename(columns={'datesampled_month_day' : 'Date'}, inplace = True)
 
     # Reads in the ASTM data which was transfered from the standard to an Excel file
-    ASTM_df = pd.read_csv('../../data/01_raw/ASTM_fuel.csv
+    ASTM_df = pd.read_csv('../../data/01_raw/ASTM_fuel.csv)
 
     # Merge the two together
     volitility_df = volitility_df.merge(ASTM_df, how='left', on='Date')
 
+    # Save to csv
     volitility_df.to_csv('../../data/03_processed/volatility_gas_ASTM.csv')
 
+    return volatility_df
 
-def date_results_df_creator(df, test):
+def date_results_df_creator(df, test_name):
     """Takes in the file path of the large combined gasoline and ASTM standards dataframe. It makes the dateStamp in datetime form and on the index. Also takes the test of the specific test to be made into a df
 
         Keyword arguments:
         df -- the string of where the combined df is stored
-        test -- the name of the test results to be tracked by date
+        test_name -- the name of the test results to be tracked by date
     """
 
     volatility_df = pd.read_csv(df)
     volatility_df['DateSampled'] = pd.to_datetime(volatility_df.DateSampled).apply(lambda x: x.date())
     volatility_df = volatility_df.set_index(volatility_df.DateSampled)
+    volatility_results_df = volatility_df[['Test','Result']]
+    volatility_results_df['Result'] = volatility_results_df.Result.replace('  ', np.nan)
+    volatility_results_df['temp'] = volatility_results_df.Result.astype('float')
+    volatility_results_df.drop('Result', axis=1, inplace=True)
+    volatility_results_floats_df = volatility_results_df.dropna()
+
+    test_df = volatility_results_floats_df[volatility_results_floats_df.Test == test_name]
+    test_df.rename(columns={"temp": "Result_deg_C"}, inplace =True)
+    testing_df = test_df.drop(columns=['Test'])
+
+    return testing_df
