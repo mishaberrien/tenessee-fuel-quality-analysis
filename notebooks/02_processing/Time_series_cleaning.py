84339a22156c6b157be554616aa91f43ccc15d1f
Kate H <katehayes.m51@gmail.com>
2019-08-02 12:59:33 -0400

    last weak on volatility ASTM function

--- notebooks/02_processing/Time_series_cleaning.py
+++ notebooks/02_processing/Time_series_cleaning.py
@@ -1,5 +1,5 @@
 def volatilty_ASTM_df_creator(routine_csv, ASTM_csv):
-    """Takes in the original csvs including the path names as strings for the results and ASTM standards. Saves the final df as 
+    """Takes in the original csvs including the path names as strings for the results and ASTM standards. Saves the final df as volatility_gas_ASTM.csv in the processed data folder.
 
         Keyword arguments:
         routine_csv -- the name of the csv where all of the annual results are saved,
